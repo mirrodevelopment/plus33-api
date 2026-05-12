6a992e1bd3f7f8f2c69cce0fe1818644058228fb
mirrodevelopment <mirrodevelopment@gmail.com>
2026-05-12 17:30:27 +0530

    update code

--- src/main/java/fr/plus33/api/service/AiRecommendationService.java
+++ src/main/java/fr/plus33/api/service/AiRecommendationService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.time.Month;
 import java.util.*;
 import java.util.stream.Collectors;
 
